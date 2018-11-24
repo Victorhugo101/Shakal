3a08533c36417dc36e02eb5c6ceb68c629579bdb
Your Name <you@example.com>
2018-11-23 22:35:15 -0300

    Tela de cadastro

--- frontend/app/src/main/java/com/example/easypark/easyparkfinal/utils/Constants.java
+++ frontend/app/src/main/java/com/example/easypark/easyparkfinal/utils/Constants.java
@@ -6,5 +6,5 @@ package com.example.easypark.easyparkfinal.utils;
 
 public class Constants {
 
-   public static final String BASE_URL = "http://192.168.0.106:9000";
+   public static final String BASE_URL = "http://192.168.1.6:9000";
 }
